f089b62c6739fc153016eecccb1044bac9955c95
dlobastov <114471478+dlobastov@users.noreply.github.com>
2023-12-23 02:38:16 +0300

    add

---
+++ lw3/src/MargheritaPizza.php
@@ -0,0 +1,12 @@
+<?php
+namespace Your\Name;
+
+class MargheritaPizza extends Pizza {
+    protected $name = 'Margherita';
+
+    public function prepare() {
+        echo "Началась готовка пиццы {$this->name}\n";
+        echo "Добавлен соус {$this->sauce}\n";
+        echo "Добавлены топпинги: " . implode(", ", $this->toppings) . "\n";
+    }
+}
